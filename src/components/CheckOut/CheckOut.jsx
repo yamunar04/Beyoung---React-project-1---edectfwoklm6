62a43aa4bde3018fb2d4d00a0120b6ee9e10d893
yamunar04 <yamunar0411@gmail.com>
2024-02-13 00:32:34 +0530

    updated files

--- src/components/CheckOut/CheckOut.jsx
+++ src/components/CheckOut/CheckOut.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import Footer from "../Footer/Footer";
 
 function CheckOut() {
     const [username, setUsername] = useState('');
@@ -19,37 +20,40 @@ function CheckOut() {
     }
 
     return (
-        <div id="main">
-            <form>
-                <label for="name">Name</label>
-                <input name="name" placeholder="Name" type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)} />
+        <>
+            <div id="checkout-main">
+                <form>
+                    <label for="name">Name</label>
+                    <input name="name" placeholder="Name" type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)} />
 
-                <label for="phoneNo">Phone Number</label>
-                <input name="phoneNo" placeholder="PhoneNumber" type="tel"
-                    value={phoneNo}
-                    onChange={(e) => setPhoneNo(e.target.value)} />
+                    <label for="phoneNo">Phone Number</label>
+                    <input name="phoneNo" placeholder="PhoneNumber" type="tel"
+                        value={phoneNo}
+                        onChange={(e) => setPhoneNo(e.target.value)} />
 
-                <label for="pincode">Pincode</label>
-                <input name="pincode" placeholder="Pincode" type="tel"
-                    value={pincode}
-                    onChange={(e) => setPincode(e.target.value)} />
+                    <label for="pincode">Pincode</label>
+                    <input name="pincode" placeholder="Pincode" type="tel"
+                        value={pincode}
+                        onChange={(e) => setPincode(e.target.value)} />
 
-                <label for="streetTown">Street/Town</label>
-                <input name="streetTown" placeholder="Street/Town" type="text"
-                    value={streetTown}
-                    onChange={(e) => setStreetTown(e.target.value)} />
+                    <label for="streetTown">Street/Town</label>
+                    <input name="streetTown" placeholder="Street/Town" type="text"
+                        value={streetTown}
+                        onChange={(e) => setStreetTown(e.target.value)} />
 
-                <label for="city">City</label>
-                <input name="city" placeholder="City" type="text"
-                    value={city}
-                    onChange={(e) => setCity(e.target.value)} />
+                    <label for="city">City</label>
+                    <input name="city" placeholder="City" type="text"
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)} />
 
-                
-                <button onClick={handleSubmit}>Place Order</button>
-            </form>
-        </div>
+
+                    <button onClick={handleSubmit}>Place Order</button>
+                </form>
+            </div>
+            <Footer />
+        </>
     )
 }
 
